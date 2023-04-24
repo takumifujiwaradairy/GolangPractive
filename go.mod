b972f45d651cb671996cb017e92da1d83a70083b
fujiwaratakumi <stepbystep0419@gmail.com>
2023-04-24 21:29:49 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,6 @@
+module GOlangDemo
+
+go 1.20
+
+//<!--Note: go mod init で指定したパッケージ名は、go.mod ファイルの module に記載される。-->
+//<!--nodeのnpm　同じようなもん-->
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+//Goでは必ずpacakge を呼び出すらしい
+
+import (
+	"fmt"
+)
+//fmtパッケージ。fmtパッケージは、標準入出力やフォーマットされたI/Oを提供するためのパッケージです。
+
+func main() {
+	fmt.Println("Golang Practice")
+}
+//Goのエントリポイントは、main関数
